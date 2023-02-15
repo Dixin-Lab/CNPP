5db41db0e648e6cddcec272ea61c7d5924342d8c
18070nbbin <1402404794@qq.com>
2023-02-15 22:04:17 +0800

    update simulator

--- Simulator/Optimal_transport.py
+++ Simulator/Optimal_transport.py
@@ -1,11 +1,10 @@
 import ot
-import matplotlib.pyplot as plt
+import argparse
 import torch
 import numpy as np
 import pickle
+
 element_max = np.vectorize(max)
-
-
 
 
 def load_data(name):
@@ -17,7 +16,6 @@ def load_data(name):
 
 
 def get_prior_P(path0, path1, epsilon=0.0001, numItermax=5000, max_time=0.0, num_seq=2000, index=1):
-
     data, num_types = load_data(path0)
 
     a_b_cost = np.load(path1)
@@ -33,13 +31,7 @@ def get_prior_P(path0, path1, epsilon=0.0001, numItermax=5000, max_time=0.0, num
                     epsilon, numItermax=numItermax)
 
     np.savez("P_" + str(len(lambda_1)) + "_prior_" + str(epsilon) + "_" +
-             str(numItermax)+"_"+str(num_seq)+"_"+str(max_time)+"_idx_"+str(index)+".npz", P=P)
-
-
-
-
-
-
+             str(numItermax) + "_" + str(num_seq) + "_" + str(max_time) + "_idx_" + str(index) + ".npz", P=P)
 
 
 def get_tpp_statistics(path="./exp_5313_5120_10000_0.05.pkl", num_seq=10000, max_time=0.05, index=0):
@@ -79,7 +71,7 @@ def get_tpp_statistics(path="./exp_5313_5120_10000_0.05.pkl", num_seq=10000, max
     cost = euclidean_distances(lambda_1, lambda_2)
 
     np.savez("lambda1_lambda2_const_" + str(len(lambda_1)) + "_" + str(num_seq) + "_" +
-             str(max_time) + "_idx"+str(index)+".npz", lambda_1=lambda_1, lambda_2=lambda_2, cost=cost)
+             str(max_time) + "_idx" + str(index) + ".npz", lambda_1=lambda_1, lambda_2=lambda_2, cost=cost)
 
 
 def get_tpp_statistics_num_seq(path="./exp_5313_5120_10000_0.05.pkl", num_seq=10000, max_time=0.05):
@@ -123,19 +115,26 @@ def get_tpp_statistics_num_seq(path="./exp_5313_5120_10000_0.05.pkl", num_seq=10
              lambda_1=lambda_1, lambda_2=lambda_2, cost=cost)
 
 
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-plk_path', type=str, default='exp_10_10_2000_30_idx')
+    parser.add_argument('-cost_path', type=str, default='lambda1_lambda2_const_10_2000_15_idx0.npz',
+                        help='the cost matrix path')
+    parser.add_argument('-max_time', type=float, default=30)
+    parser.add_argument('-num_seq', type=int, default=2000)
+    parser.add_argument('-numItermax', type=int, default=10000, help='max num iteration for sinkhorn algorithm')
+    parser.add_argument('-epsilon', type=int, default=0.0001, help='epsilon for sinkhorn algorithm')
+    parser.add_argument('-index', type=int, default=0, help='give a index to the prior P')
+    parser.add_argument('-out_put', type=str, default='.')
+
+    opt = parser.parse_known_args()[0]
+
+    get_tpp_statistics(path=opt.plk_path, num_seq=opt.num_seq,
+                       max_time=opt.max_time, index=opt.index)
+    get_prior_P(path0=opt.plk_path, path1=opt.cost_path, epsilon=opt.epsilon,
+                numItermax=opt.numItermax, max_time=opt.max_time, num_seq=opt.num_seq
+                , index=opt.index)
+
+
 if __name__ == "__main__":
-
-    path = ("./Tpp_data/Syn_data/unweighted/exp/exp_50_50_2000_6_idx", 6, 'lambda1_lambda2_const_50_2000_6_',
-            "./Tpp_data/Syn_data/unweighted/prior/P_50_prior_0.0001_10000_2000_6_idx_")
-    for idx in range(5):
-        plk_path = path[0]+str(idx)+".pkl"
-        max_time = path[1]
-        get_tpp_statistics(path=plk_path, num_seq=10000,
-                           max_time=max_time, index=idx)
-
-        abpath = path[2]+"idx"+str(idx)+".npz"
-
-        get_prior_P(path0=plk_path, path1=abpath, epsilon=0.0001,
-                    numItermax=10000, max_time=max_time, num_seq=10000, index=idx)
-        Ppath = path[3]+str(idx)+".npz"
-
+    main()
